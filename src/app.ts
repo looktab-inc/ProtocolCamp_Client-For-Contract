f221ec80b72e3e7f9e8238d1f78a83f6446d095c
TaeGit <tgsong827@gmail.com>
2023-05-16 17:58:19 +0900

    add initialize method for TinjiContract

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Wallet, web3 } from "@project-serum/anchor";
 import { getTinjiProgram, getTinjiProvider } from "./config/config";
 import bankSecret from "./secret/my-wallet.json";
+import { TinjiContract } from "./tinjiClient";
 
 async function main() {
   // 0. my wallet
@@ -10,6 +11,17 @@ async function main() {
   // 1. Provider and Contract Program
   const tinjiProvider = await getTinjiProvider(bankWallet);
   const tinjiProgram = await getTinjiProgram(tinjiProvider);
+
+  const tinjiContract = new TinjiContract(
+    tinjiProvider,
+    tinjiProgram,
+    bankKeypair
+  );
+
+  // 2. create Bank Account and Initialize
+  const bankAccount = web3.Keypair.generate();
+  const tx = await tinjiContract.initializeContract(bankAccount);
+  console.log(tx);
 }
 
 main();
--- src/tinjiClient.ts
+++ src/tinjiClient.ts
@@ -1,19 +1,46 @@
-import { AnchorProvider, Program } from "@project-serum/anchor";
-import {
-  withdrawForExpired,
-  withdrawForVerified,
-} from "./contract/instructions";
+import * as anchor from "@project-serum/anchor";
+import { AnchorProvider, Program, web3 } from "@project-serum/anchor";
+import { DepositForNft } from "./idl/deposit_for_nft";
 
 export class TinjiContract {
   readonly provider: AnchorProvider;
-  readonly program: Program;
+  readonly program: Program<DepositForNft>;
+  private bankWallet: web3.Keypair;
 
-  constructor(provider: AnchorProvider, program: Program) {
+  readonly PDA_BASE_SEED: Uint8Array =
+    anchor.utils.bytes.utf8.encode("pda-auth");
+  readonly SOL_VAULT_BASE_SEED: Uint8Array =
+    anchor.utils.bytes.utf8.encode("sol-vault");
+
+  constructor(
+    provider: AnchorProvider,
+    program: Program<DepositForNft>,
+    bankWallet: web3.Keypair
+  ) {
     this.provider = provider;
     this.program = program;
+    this.bankWallet = bankWallet;
   }
 
-  initialize() {}
+  async initializeContract(bankAccount: web3.Keypair): Promise<string> {
+    let [pdaAuth, _pdaBump] = web3.PublicKey.findProgramAddressSync(
+      [this.PDA_BASE_SEED, bankAccount.publicKey.toBuffer()],
+      this.program.programId
+    );
+
+    const transaction = await this.program.methods
+      .initialize()
+      .accounts({
+        bankAccount: bankAccount.publicKey,
+        pdaAuth: pdaAuth,
+        bankAuth: this.provider.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([bankAccount, this.bankWallet])
+      .rpc();
+
+    return transaction;
+  }
 
   depositForNFT() {}
 
