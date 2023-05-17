b3a29aa4e4ddc7a56ac6033a39e48a8a4ccbdc21
TaeGit <tgsong827@gmail.com>
2023-05-17 11:07:05 +0900

    add deposit and withraw for burned

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Wallet, web3 } from "@project-serum/anchor";
 import { getTinjiProgram, getTinjiProvider } from "./config/config";
 import bankSecret from "./secret/my-wallet.json";
-import { TinjiContract } from "./tinjiClient";
+import { TinjiContract } from "./modules/tinjiClient";
 
 async function main() {
   // 0. my wallet
@@ -19,9 +19,26 @@ async function main() {
   );
 
   // 2. create Bank Account and Initialize
+  console.log("\n[01. Initialize]");
   const bankAccount = web3.Keypair.generate();
-  const tx = await tinjiContract.initializeContract(bankAccount);
-  console.log(tx);
+  const tx1 = await tinjiContract.initializeContract(bankAccount);
+  console.log(tx1);
+
+  // 3. deposit for nft
+  console.log("\n[02. Deposit for a NFT]");
+  const tx2 = await tinjiContract.depositForNFT(bankAccount);
+  console.log(tx2);
+
+  // 4. withdraw for burned
+  console.log("\n[03. Withdraw for Burned]");
+  const clientAccount = web3.Keypair.generate();
+  const tx3 = await tinjiContract.withdrawForBurned(
+    bankAccount,
+    clientAccount.publicKey
+  );
+  console.log(tx3);
+  console.log(`BankWallet Address: ${bankWallet.publicKey.toString()}`);
+  console.log(`Client Address: ${clientAccount.publicKey.toString()}`);
 }
 
 main();
---
+++ src/modules/tinjiClient.ts
@@ -0,0 +1,103 @@
+import * as anchor from "@project-serum/anchor";
+import { AnchorProvider, Program, web3 } from "@project-serum/anchor";
+import { DepositForNft } from "../idl/deposit_for_nft";
+
+export class TinjiContract {
+  readonly provider: AnchorProvider;
+  readonly program: Program<DepositForNft>;
+  private bankWallet: web3.Keypair;
+
+  readonly PDA_BASE_SEED: Uint8Array =
+    anchor.utils.bytes.utf8.encode("pda-auth");
+  readonly SOL_VAULT_BASE_SEED: Uint8Array =
+    anchor.utils.bytes.utf8.encode("sol-vault");
+
+  constructor(
+    provider: AnchorProvider,
+    program: Program<DepositForNft>,
+    bankWallet: web3.Keypair
+  ) {
+    this.provider = provider;
+    this.program = program;
+    this.bankWallet = bankWallet;
+  }
+
+  async initializeContract(bankAccount: web3.Keypair): Promise<string> {
+    const pdaAuthPubkey = this.getPdaAuthPubkey(bankAccount.publicKey);
+
+    const transaction = await this.program.methods
+      .initialize()
+      .accounts({
+        bankAuth: this.bankWallet.publicKey,
+        bankAccount: bankAccount.publicKey,
+        pdaAuth: pdaAuthPubkey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([bankAccount, this.bankWallet])
+      .rpc();
+
+    return transaction;
+  }
+
+  async depositForNFT(bankAccount: web3.Keypair): Promise<string> {
+    const pdaAuthPubkey = this.getPdaAuthPubkey(bankAccount.publicKey);
+    const solVaultPubkey = this.getSolVaultPubkey(pdaAuthPubkey);
+
+    const transaction = await this.program.methods
+      .depositForNft()
+      .accounts({
+        bankAuth: this.bankWallet.publicKey,
+        bankAccount: bankAccount.publicKey,
+        pdaAuth: pdaAuthPubkey,
+        solVault: solVaultPubkey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([this.bankWallet])
+      .rpc();
+
+    return transaction;
+  }
+
+  async withdrawForBurned(
+    bankAccount: web3.Keypair,
+    clientAddress: web3.PublicKey
+  ): Promise<string> {
+    const pdaAuthPubKey = this.getPdaAuthPubkey(bankAccount.publicKey);
+    const solVaultPubkey = this.getSolVaultPubkey(pdaAuthPubKey);
+
+    const transaction = await this.program.methods
+      .withdrawForBurned()
+      .accounts({
+        bankAuth: this.bankWallet.publicKey,
+        bankAccount: bankAccount.publicKey,
+        pdaAuth: pdaAuthPubKey,
+        solVault: solVaultPubkey,
+        systemProgram: web3.SystemProgram.programId,
+        clientAccount: clientAddress,
+      })
+      .signers([this.bankWallet])
+      .rpc();
+
+    return transaction;
+  }
+
+  withdrawForExpired() {}
+
+  withdrawForVerified() {}
+
+  private getPdaAuthPubkey(bankAccountPubkey: web3.PublicKey): web3.PublicKey {
+    let [pdaAuthPubKey, _pdaBump] = web3.PublicKey.findProgramAddressSync(
+      [this.PDA_BASE_SEED, bankAccountPubkey.toBuffer()],
+      this.program.programId
+    );
+    return pdaAuthPubKey;
+  }
+
+  private getSolVaultPubkey(pdaAuthPubkey: web3.PublicKey): web3.PublicKey {
+    let [solVaultPubkey, _solVaultBump] = web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode("sol-vault"), pdaAuthPubkey.toBuffer()],
+      this.program.programId
+    );
+    return solVaultPubkey;
+  }
+}
