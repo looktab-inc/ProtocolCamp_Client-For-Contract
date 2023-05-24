7d0b3ec395dc560f90e66d39dfd287f1970709fe
TaeGit <tgsong827@gmail.com>
2023-05-24 15:34:12 +0900

    add mintNft method to TinjiNft class

--- src/modules/tinjiNft.ts
+++ src/modules/tinjiNft.ts
@@ -1,10 +1,15 @@
 import * as umilib from "@metaplex-foundation/umi";
 import { Umi, createSignerFromKeypair } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import { mintV2, mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 import { nftStorageUploader } from "@metaplex-foundation/umi-uploader-nft-storage";
 import { web3 } from "@project-serum/anchor";
 import { signerIdentity } from "@metaplex-foundation/umi";
+import { generateSigner } from "@metaplex-foundation/umi";
+import { transactionBuilder } from "@metaplex-foundation/umi";
+import { setComputeUnitLimit } from "@metaplex-foundation/mpl-essentials";
+import { TinjiContract } from "./tinjiContract";
+import { getTinjiProgram, getTinjiProvider } from "../config/config";
 
 
 export class TinjiNft {
@@ -31,6 +36,42 @@ export class TinjiNft {
         return createUmi(network)
           .use(mplCandyMachine())
           .use(nftStorageUploader());
+    }
+
+    async mintNft(
+        candyMachineAddress: umilib.PublicKey,
+        collectionNftAddress: umilib.PublicKey,
+        bankAccountAddress: web3.PublicKey,
+    ): Promise<umilib.PublicKey | null> {
+        const nftMint = generateSigner(this.umi);
+
+        // mint NFT from CandyMachine.
+        const txResult = await transactionBuilder()
+            .add(setComputeUnitLimit(this.umi, { units: 800_000 }))
+            .add(
+                mintV2(this.umi, {
+                  candyMachine: candyMachineAddress,
+                  nftMint: nftMint,
+                  collectionMint: collectionNftAddress,
+                  collectionUpdateAuthority: this.umi.identity.publicKey,
+                })
+            ).sendAndConfirm(this.umi);
+        
+        // TODO : Deposit Sol into Tinji Contract.
+        const tinjiProvider = await getTinjiProvider(this.bankWallet, "http://127.0.0.1:8899");
+        const tinjiProgram = await getTinjiProgram(tinjiProvider);
+        const tinjiContract = new TinjiContract(tinjiProvider, tinjiProgram, this.bankWallet);
+
+        const txSignature = await tinjiContract.depositForNFT(bankAccountAddress);
+
+        console.log(txSignature);
+        
+        // console.log(txResult);
+        if (txResult.result.value.err == null) {
+            return nftMint.publicKey;
+        } else {
+            return null;
+        }
     }
 
 
